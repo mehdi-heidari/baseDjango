5b258a2b2c4980f7ce4eead4fef57f81f7972d72
heidari <heidari.m84@gmail.com>
2023-09-17 20:38:20 +0330

    Show img in temaplates

--- baseDjango/urls.py
+++ baseDjango/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,11 @@ urlpatterns = [
 urlpatterns += [
     path('', include(('sample.urls', 'sample'), namespace='sample')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
